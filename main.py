7a27b026ea68bc6d36f63dfe1a5ae52fd76a62d3
16792823 <156852895+16792823@users.noreply.github.com>
2024-01-21 12:00:50 -0300

    commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Meu Site Streamlit")
+
+st.subheader("CEZÁRIOS CONSULTORIA E TREINAMENTOS.")
+
+st.title("Autenticaçõa de Certificado:")
+st.write("Deseja verificar?[Clique aqui](http://web-production-edc4e.up.railway.app)")
